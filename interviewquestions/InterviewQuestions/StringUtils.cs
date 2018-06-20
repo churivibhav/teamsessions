ecee0deceb796cb0f0948f226d261741469d207e
Vibhav Churi <churi.vibhav@gmail.com>
2018-06-21 00:16:40 +0530

    Reverse String

---
+++ interviewquestions/InterviewQuestions/StringUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace InterviewQuestions
+{
+    public class StringUtils
+    {
+        public static string Reverse(string str)
+        {
+            return string.Join(string.Empty, str.ToCharArray().Reverse());
+        }
+    }
+}
